a333132179084cf5bf858327a1a525e281061333
evramfady <91060850+evramfady@users.noreply.github.com>
2021-12-07 19:23:38 +0200

    .

---
+++ avoiding car/HAL/Motor_Driver/motor_driver.c
@@ -0,0 +1,6 @@
+/*
+ * motor_driver.c
+ *
+ * Created: 12/7/2021 7:17:15 PM
+ *  Author: evram
+ */ 
---
+++ avoiding car/HAL/Motor_Driver/motor_driver.h
@@ -0,0 +1,16 @@
+/*
+ * motor_driver.h
+ *
+ * Created: 12/7/2021 7:18:29 PM
+ *  Author: evram
+ */ 
+
+
+#ifndef MOTOR_DRIVER_H_
+#define MOTOR_DRIVER_H_
+
+
+
+
+
+#endif /* MOTOR_DRIVER_H_ */
